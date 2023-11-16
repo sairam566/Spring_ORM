be6b13c83b7c3a616f7b41bdf0b3e478976ee827
sairam566 <sairam1431995@gmail.com>
2023-11-16 10:55:53 +0530

    Entity class

---
+++ 01_Spring_Integration_With_Hibernate/src/main/java/com/tsr/entities/Vehicle.java
@@ -0,0 +1,95 @@
+package com.tsr.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "vehicle")
+public class Vehicle {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int vehicleNo;
+	@Column(name = "model_name")
+	private String modelName;
+	private String manufacturer;
+	private String color;
+	@Column(name = "fuel_type")
+	private String fuelType;
+	@Column(name = "lease_amount")
+	private double leaseAmount;
+	
+	public Vehicle() {
+		super();
+	}
+
+	public Vehicle(int vehicleNo, String modelName, String manufacturer, String color, String fuelType,
+			double leaseAmount) {
+		super();
+		this.vehicleNo = vehicleNo;
+		this.modelName = modelName;
+		this.manufacturer = manufacturer;
+		this.color = color;
+		this.fuelType = fuelType;
+		this.leaseAmount = leaseAmount;
+	}
+
+	public int getVehicleNo() {
+		return vehicleNo;
+	}
+
+	public void setVehicleNo(int vehicleNo) {
+		this.vehicleNo = vehicleNo;
+	}
+
+	public String getModelName() {
+		return modelName;
+	}
+
+	public void setModelName(String modelName) {
+		this.modelName = modelName;
+	}
+
+	public String getManufacturer() {
+		return manufacturer;
+	}
+
+	public void setManufacturer(String manufacturer) {
+		this.manufacturer = manufacturer;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public String getFuelType() {
+		return fuelType;
+	}
+
+	public void setFuelType(String fuelType) {
+		this.fuelType = fuelType;
+	}
+
+	public double getLeaseAmount() {
+		return leaseAmount;
+	}
+
+	public void setLeaseAmount(double leaseAmount) {
+		this.leaseAmount = leaseAmount;
+	}
+
+	@Override
+	public String toString() {
+		return "Vehicle [vehicleNo=" + vehicleNo + ", modelName=" + modelName + ", manufacturer=" + manufacturer
+				+ ", color=" + color + ", fuelType=" + fuelType + ", leaseAmount=" + leaseAmount + "]";
+	}
+	
+}
