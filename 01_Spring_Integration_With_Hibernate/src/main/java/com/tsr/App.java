94cf6b225a689df1d3332c9927f90d399e2f297e
sairam566 <sairam1431995@gmail.com>
2023-11-17 13:33:51 +0530

    Main class

--- 01_Spring_Integration_With_Hibernate/src/main/java/com/tsr/App.java
+++ 01_Spring_Integration_With_Hibernate/src/main/java/com/tsr/App.java
@@ -1,10 +1,19 @@
 package com.tsr;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.tsr.bo.VehicleBO;
+import com.tsr.config.RootConfig;
+import com.tsr.service.VehicleService;
 
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	ApplicationContext context = new AnnotationConfigApplicationContext(RootConfig.class);
+    	VehicleService service = context.getBean("vehicleService",VehicleService.class);
+    	VehicleBO vehicle = service.getVehicle(1);
+    	System.out.println(vehicle);
     }
 }
