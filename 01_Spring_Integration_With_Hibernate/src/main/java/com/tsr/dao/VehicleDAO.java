f13b64ce2caef753bb930d9af56965eb86d5c98d
sairam566 <sairam1431995@gmail.com>
2023-11-17 13:33:05 +0530

    Vehicle DAO class

--- 01_Spring_Integration_With_Hibernate/src/main/java/com/tsr/dao/VehicleDAO.java
+++ 01_Spring_Integration_With_Hibernate/src/main/java/com/tsr/dao/VehicleDAO.java
@@ -4,9 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.orm.hibernate5.HibernateTemplate;
 import org.springframework.stereotype.Repository;
 
+import com.tsr.entities.Vehicle;
+
 @Repository
 public class VehicleDAO {
 	
 	@Autowired
 	private HibernateTemplate hibernateTemplate;
+	
+	public Vehicle getVehicle(int vehicleNo) {
+		return hibernateTemplate.get(Vehicle.class, vehicleNo);
+	}
 }
