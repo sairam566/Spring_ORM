2577255260937826aa31de414fb803bec881b27a
sairam566 <sairam1431995@gmail.com>
2023-11-30 13:48:40 +0530

    Product BO

---
+++ 04_Spring_Data_JPA/src/main/java/com/tsr/bo/ProductBo.java
@@ -0,0 +1,49 @@
+package com.tsr.bo;
+
+public class ProductBo {
+
+	private int id;
+	private String name;
+	private double price;
+
+	public ProductBo() {
+		super();
+	}
+
+	public ProductBo(int id, String name, double price) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.price = price;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	@Override
+	public String toString() {
+		return "ProductBo [id=" + id + ", name=" + name + ", price=" + price + "]";
+	}
+
+}
