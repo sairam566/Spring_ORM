964f28521ea42406f3ac9e35c7da47d59c6d7745
sairam566 <sairam1431995@gmail.com>
2023-12-03 13:18:23 +0530

    Changed extended repository to JPARepository to extend functionality

--- 04_Spring_Data_JPA/src/main/java/com/tsr/repositorys/ProductsRepository.java
+++ 04_Spring_Data_JPA/src/main/java/com/tsr/repositorys/ProductsRepository.java
@@ -1,9 +1,13 @@
 package com.tsr.repositorys;
 
-import org.springframework.data.repository.CrudRepository;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.tsr.entities.Product;
 
-public interface ProductsRepository extends CrudRepository<Product, Integer>{
+public interface ProductsRepository extends JpaRepository<Product, Integer> {
+	public List<Product> findByName(String name);
 
+	public List<Product> findByPriceBetween(double price1,double price2);
 }
