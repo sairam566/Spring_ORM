1bcb08cb2f4afc24bd500b2449bd45e97dc2e2a9
sairam566 <sairam1431995@gmail.com>
2023-12-05 12:19:00 +0530

    DeliveryAssociate Repository

---
+++ 04_Spring_Data_JPA/src/main/java/com/tsr/repositorys/DeliveryAssociateRepository.java
@@ -0,0 +1,14 @@
+package com.tsr.repositorys;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.tsr.entities.DeliveryAssociate;
+
+public interface DeliveryAssociateRepository extends JpaRepository<DeliveryAssociate, Integer> {
+
+	@Query(value = "from DeliveryAssociate del where experience >= ?1")
+	public List<DeliveryAssociate> getDeliveryByWeight(int weight);
+}
