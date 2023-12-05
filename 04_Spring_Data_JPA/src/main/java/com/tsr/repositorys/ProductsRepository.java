db7ba25d0401925dc0f5e8088608d67a4c97e9fd
sairam566 <sairam1431995@gmail.com>
2023-12-05 12:20:34 +0530

    Product Repository
    - @Query
    - @Param
    - @Modifying

--- 04_Spring_Data_JPA/src/main/java/com/tsr/repositorys/ProductsRepository.java
+++ 04_Spring_Data_JPA/src/main/java/com/tsr/repositorys/ProductsRepository.java
@@ -3,6 +3,9 @@ package com.tsr.repositorys;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.tsr.entities.Product;
 
@@ -10,4 +13,14 @@ public interface ProductsRepository extends JpaRepository<Product, Integer> {
 	public List<Product> findByName(String name);
 
 	public List<Product> findByPriceBetween(double price1,double price2);
+	
+	@Query(value="from Product p where p.id = ?1")
+	public Product findByid(int id);
+	
+	@Query(value="from Product p where p.name like :pname")
+	public List<Product> findByNameLike(@Param("pname") String name);
+	
+	@Modifying
+	@Query(value="update Product p set p.name = :name where p.id = :id")
+	public int updateProductNameById(@Param("name") String name, @Param("id") int id);
 }
