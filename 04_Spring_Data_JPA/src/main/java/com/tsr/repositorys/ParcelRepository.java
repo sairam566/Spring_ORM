71a62a5f60fb442272057f1bdf1a7f1f7ab9521c
sairam566 <sairam1431995@gmail.com>
2023-12-06 09:39:02 +0530

    New Parcel Repository

---
+++ 04_Spring_Data_JPA/src/main/java/com/tsr/repositorys/ParcelRepository.java
@@ -0,0 +1,14 @@
+package com.tsr.repositorys;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.tsr.entities.Parcel;
+
+public interface ParcelRepository extends JpaRepository<Parcel, Integer>{
+	
+	@Query(value="from Parcel p where p.deliveryAssociate.associateName = ?1")
+	public List<Parcel> getAllParcelDeliveredByAssociate(String name);
+}
