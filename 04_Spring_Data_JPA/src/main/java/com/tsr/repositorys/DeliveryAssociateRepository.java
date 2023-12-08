62d262b0fa3eb19fe928983cb2cfce851ebabf05
sairam566 <sairam1431995@gmail.com>
2023-12-08 12:07:04 +0530

    Closed Projection

--- 04_Spring_Data_JPA/src/main/java/com/tsr/repositorys/DeliveryAssociateRepository.java
+++ 04_Spring_Data_JPA/src/main/java/com/tsr/repositorys/DeliveryAssociateRepository.java
@@ -6,10 +6,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import com.tsr.entities.DeliveryAssociate;
+import com.tsr.entities.DeliveryAssociateContactView;
 
 public interface DeliveryAssociateRepository extends JpaRepository<DeliveryAssociate, Integer> {
 
 	@Query(value = "from DeliveryAssociate del where experience >= ?1")
 	public List<DeliveryAssociate> getDeliveryAssociateByAge(int age);
 	
+	//Closed Projection
+	public List<DeliveryAssociateContactView> findByExperienceEquals(int exp);
 }
